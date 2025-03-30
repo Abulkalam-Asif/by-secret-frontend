b7fef00e002156ba249fed263d9216162b9bef15
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2025-03-30 08:36:18 +0500

    cookies issue resolved

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,16 +1,35 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+// Error handling link
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+// HTTP link with proper credentials
+const httpLink = new HttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+  credentials: "include",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
-    credentials: "include",
-  }),
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
       fetchPolicy: "cache-and-network",
     },
   },
+  credentials: "include",
 });
 
 export default client;
